8e3255a579bbe33db86b9847ecaf711580ca6399
Inggo <inggo.espinosa@gmail.com>
2016-08-16 02:59:17 +0800

    Remove Installer

--- classes/Loader.php
+++ classes/Loader.php
@@ -6,10 +6,6 @@ class Loader
 {
     public function __construct($plugin_dir)
     {
-        if (!class_exists('Inggo\CF1971\Contests\Installer')) {
-            require_once($plugin_dir . 'classes/Installer.php');
-        }
-
         if (!class_exists('Inggo\CF1971\Contests\Shortcodes\Contest')) {
             require_once($plugin_dir . 'classes/Shortcodes/Contest.php');
         }
