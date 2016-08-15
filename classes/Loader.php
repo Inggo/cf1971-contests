030674e80c7b7c0d3752ef2e691c9ec5c4ed08de
Inggo <inggo.espinosa@gmail.com>
2016-08-16 03:10:59 +0800

    Register Team CPT

--- classes/Loader.php
+++ classes/Loader.php
@@ -11,19 +11,34 @@ class Loader
         }
 
         \add_action('init', [$this, 'registerContestCPT']);
+        \add_action('init', [$this, 'registerTeamCPT']);
     }
 
     public function registerContestCPT()
     {
-        \register_post_type('contests', [
+        \register_post_type('cf1971_contests', [
             'labels' => [
                 'name' => \__('Contests', 'cf1971-contests'),
                 'singular_name' => \__('Contest', 'cf1971-contests'),
             ],
-            'public' => true,
+            'public' => false,
             'has_archive' => false,
             'capability_type' => 'page',
-            'supports' => ['title', 'editor', 'thumbnail', 'revisions', 'custom-fields'],
+            'supports' => ['title', 'editor', 'thumbnail', 'revisions'],
+        ]);
+    }
+
+    public function registerTeamCPT()
+    {
+        \register_post_type('cf1971_contestants', [
+            'labels' => [
+                'name' => \__('Contestants', 'cf1971-contests'),
+                'singular_name' => \__('Contestant', 'cf1971-contests'),
+            ],
+            'public' => false,
+            'has_archive' => false,
+            'capability_type' => 'page',
+            'supports' => ['title', 'thumbnail', 'revisions'],
         ]);
     }
 }
